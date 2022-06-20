a0cb602d9bd837f28d7fc2391ce7d9b88abd1036
Rafał Dymek <rafal.dymek93@gmail.com>
2022-06-20 09:15:00 +0200

    Add_rand_fun

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var computerMove, playermove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playermove = ' Papier';
-printMessage('Zagrałem' + playermove + '! Jeżeli Twoj ruch to kamień, to przegrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
