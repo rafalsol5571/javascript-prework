b88e412d4801d694fd00da0916068a82c4092284
Rafał Dymek <rafal.dymek93@gmail.com>
2022-06-20 08:51:33 +0200

    js gra

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playermove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playermove = ' Papier';
+printMessage('Zagrałem' + playermove + '! Jeżeli Twoj ruch to kamień, to przegrywasz!');
